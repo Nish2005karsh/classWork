c00e484e806d2df296f14f855641aa1f06770bfd
Nish2005karsh <nishkarshpandey059@gmail.com>
2026-02-11 10:02:08 +0530

    Learned OS

--- frontend/frontend/server.js
+++ frontend/frontend/server.js
@@ -178,8 +178,12 @@
 // catch(err){
 //     console.log('Error found ',err);
 // }
-// import os from 'os';
-// console.log(os.platform());
-// console.log(os.userInfo());
-// console.log(os.arch());
-// console.log(os.cpus());
+import os from 'os';
+console.log(os.platform());
+console.log(os.userInfo());
+console.log(os.arch());
+console.log(os.cpus());
+console.log(os.freemem());
+console.log(os.totalmem());
+console.log(os.uptime());
+console.log(os.homedir());
